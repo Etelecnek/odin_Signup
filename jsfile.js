5da7a987c5fb97c0725869fff1c4b2c0cb72db1a
Etelecnek <126229297+Etelecnek@users.noreply.github.com>
2023-05-10 11:26:18 -0700

    Add validation for email phone number, make validations change border color

--- jsfile.js
+++ jsfile.js
@@ -1,17 +1,23 @@
 function validate() {
   var x = document.getElementById("password");
   var y = document.getElementById("confirm_password");
-  if(x.value == y.value && x.value && y.value != "") {
+  if(x.value == y.value && y.value != "") {
     document.getElementById("message").style.color = "green";
     document.getElementById("message").innerHTML = "Matching";
-  } else if (x.value != y.value) {
+    document.getElementById("password").className = document.getElementById("password").className.replace + " match";
+    document.getElementById("confirm_password").className = document.getElementById("confirm_password").className.replace + " match";
+  } else if (x.value != y.value && y.value != "") {
     document.getElementById("message").style.color = "red";
     document.getElementById("message").innerHTML = "Not Matching";
+    document.getElementById("password").className = document.getElementById("password").className.replace + " mismatch";
+    document.getElementById("confirm_password").className = document.getElementById("confirm_password").className.replace + " mismatch";
   } else {
     document.getElementById("message").innerHTML = "";
+    document.getElementById("password").className = document.getElementById("password").className.replace + "";
+    document.getElementById("confirm_password").className = document.getElementById("confirm_password").className.replace + "";
   }
 }
 
 function createAccount() {
-  alert("I'm clicked");
+
 }
